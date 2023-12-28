32b9d143bcb93e838b27432ce955486fa42122ee
Dilan Dilruksha <dilandilruksha0gmail.com>
2023-12-28 10:53:26 +0530

    complete sign in function

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -21,9 +21,9 @@ export const signin = async (req, res, next) => {
   const { username, password } = req.body;
   try {
     const validUser = await User.findOne({ username  });
-    if (!validUser) return next(errorHandler(401, "Username not found"));
+    if (!validUser) return next(errorhandler(401, "Username not found"));
     const validPassword =  bcryptjs.compareSync(password, validUser.password);
-    if (!validPassword) return next(errorHandler(401, "Invalid password"));
+    if (!validPassword) return next(errorhandler(401, "Invalid password"));
     const token = jwt.sign({ userId: validUser._id }, process.env.JWT_SECRET);
     const { password: userPassword, ...rest } = validUser._doc;
     res.cookie("token", token, { httpOnly: true })
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,9 +1,77 @@
+import { set } from 'mongoose'
 import React from 'react'
+import { Link , useNavigate} from 'react-router-dom'
 
-function SignIn() {
+function Signin() {
+  const [formData, setFormData] = React.useState({})
+  const [error, setError] = React.useState(null)
+  const [loading, setLoading] = React.useState(false)
+  const navigate = useNavigate()
+
+  const handleChange = (e) => {
+    if (e.target.type === 'radio') {
+      setFormData({ ...formData, gender: e.target.value });
+    } else {
+      setFormData({ ...formData, [e.target.id]: e.target.value });
+    }
+  };
+  
+  const handleSubmit =  async(e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+    const res = await fetch('/api/auth/signin',
+      {
+        method: 'POST',
+        body: JSON.stringify(formData),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+    );
+    const data = await res.json();
+    if (data.success === false) {
+      setLoading(false);
+      setError(data.message);
+      return;
+    }
+    setLoading(false);
+    console.log(data);
+    setError(null);
+    navigate('/');
+    }catch (err) {
+      console.log(err);
+      setError(err.message);
+      setLoading(false);
+    }
+    
+  }
+
+  console.log(formData)
   return (
-    <div>SignIn</div>
+    <div className='p-5 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-bold my-10'>Sign In</h1>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-5'>
+        <input type="text" placeholder='username'
+          className='border p-3 rounded-lg ' id='username' onChange={handleChange}/>
+
+        <input type="password" placeholder=' password'
+          className='border p-3 rounded-lg ' id='password'onChange={handleChange} />
+
+        <button disabled={loading} className='bg-blue-500 text-white p-3 rounded-lg uppercase hover:opacity-70 disabled:opacity-50'>
+        {loading ? 'Loading...' : 'Sign In'}
+        </button>
+
+      </form>
+      <div className='flex justify-center gap-2 mt-5'>
+        <p>Don t have an account? </p>
+        <Link to= {"/sign-up"} >
+          <span className='text-blue-500'>Sign Up</span>
+          </Link>
+      </div>
+      {error && <p className='text-red-500 mt-5 text-center'>{error}</p>}
+    </div>
   )
 }
 
-export default SignIn
+export default Signin
