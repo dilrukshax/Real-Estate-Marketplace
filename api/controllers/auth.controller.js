eb8d991b3d7c600dcf0ca2c83e470dedb5e4a83c
Dilan Dilruksha <dilandilruksha0gmail.com>
2023-12-28 10:32:53 +0530

    crate sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorhandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 
 export const signup = async (req, res, next) => {
@@ -13,6 +15,26 @@ export const signup = async (req, res, next) => {
 } catch (error) {
     next(error);
   }
-  };
+};
+
+export const signin = async (req, res, next) => {
+  const { username, password } = req.body;
+  try {
+    const validUser = await User.findOne({ username  });
+    if (!validUser) return next(errorHandler(401, "Username not found"));
+    const validPassword =  bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Invalid password"));
+    const token = jwt.sign({ userId: validUser._id }, process.env.JWT_SECRET);
+    const { password: userPassword, ...rest } = validUser._doc;
+    res.cookie("token", token, { httpOnly: true })
+    .status(200)
+    .json(validUser);
+    
+  } catch (error) {
+    next(error);
+    
+  }
+
+}
   
 
