97af722de1a17775c2982c45941772cb6d1674ee
Dilan Dilruksha <dilandilruksha0gmail.com>
2024-01-12 17:31:11 +0530

    Create use update API

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,4 +1,46 @@
-export const test = (req, res)=> {
-            res.send('Api is working properly');
+import bcryptjs from "bcryptjs";
+import User from "../models/user.model.js";
+import { errorhandler } from "../utils/error.js";
+
+
+export const test = (req, res) => {
+    res.json({
+        message: "Api route works"
+    });
+};
+
+export const updateUser = async (req, res, next) => {
+    if (req.user.id !== req.params.id)
+        return next(errorhandler(401, "you can only update your account"));
+    try {
+        if (req.body.password) {
+            req.body.password = bcryptjs.hashSync(
+                req.body.password,
+                10
+            );
         }
+
+        const updateUser = await User.findByIdAndUpdate(
+
+            req.params.id,
+            {
+                $set: {
+                    name: req.body.name,
+                    email: req.body.email,
+                    contact: req.body.contact,
+                    username: req.body.username,
+                    password: req.body.password,
+                    avatar: req.body.avatar,
+                }
+            }, { new: true })
+
+        const { password, ...rest } = updateUser._doc;
+
+        res.status(200).json(rest);
+
+    } catch (error) {
+        next(error);
+    }
+
+}
 
--- api/index.js
+++ api/index.js
@@ -3,11 +3,15 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoute from './routes/user.route.js';
 import authRoute from './routes/auth.route.js';
+import cookieParser from "cookie-parser";
+
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cookieParser());
 
 mongoose.connect(process.env.MONGODB_URI).then(() => {
     console.log('Connected to MongoDB!');
